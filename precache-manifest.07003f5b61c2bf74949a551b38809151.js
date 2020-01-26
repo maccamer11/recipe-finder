7a65128cfc4ed8f3005476b9449ee2f78d4fd5f5
Das <maccameron@live.ca>
2020-01-26 03:19:20 -0700

    Updates

---
+++ precache-manifest.07003f5b61c2bf74949a551b38809151.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78752bb9f222847c8c0628c5399a993d",
+    "url": "/recipe-finder/index.html"
+  },
+  {
+    "revision": "5d65e24a6597b93bd047",
+    "url": "/recipe-finder/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "cdd245d9b7e49921c2bd",
+    "url": "/recipe-finder/static/css/main.7272b803.chunk.css"
+  },
+  {
+    "revision": "5d65e24a6597b93bd047",
+    "url": "/recipe-finder/static/js/2.af867525.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipe-finder/static/js/2.af867525.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cdd245d9b7e49921c2bd",
+    "url": "/recipe-finder/static/js/main.9901769d.chunk.js"
+  },
+  {
+    "revision": "95212c06fc5473fc80d5",
+    "url": "/recipe-finder/static/js/runtime-main.b84caa1e.js"
+  },
+  {
+    "revision": "f5ed337ea222244bb684d065b76ce6d6",
+    "url": "/recipe-finder/static/media/vectorstock_10137835.f5ed337e.png"
+  }
+]);
